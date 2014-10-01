e25ce131f8dc29c76236c390aa5c4e7cc4dcb729
Eduard Erja <eduard.erja@gmail.com>
2014-10-01 23:16:04 +0300

    refactoring and tests

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,43 +4,30 @@ from django.template import RequestContext, loader
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 from django.core.urlresolvers import reverse
+from django.views import generic
+from django.utils import timezone
 
 # Create your views here.
 
 from polls.models import Poll, Choice
 
-def index(request):
-	latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
-	template = loader.get_template('polls/index.html')
-	context = RequestContext(request, {
-		'latest_poll_list': latest_poll_list
-		})
+class IndexView(generic.ListView):
+	template_name = 'polls/index.html'
+	context_object_name = 'latest_poll_list'
+
+	def get_queryset(self):
+		"""Return the last five published polls. (not including those set to be
+    	published in the future)."""
+		return Poll.objects.filter(pub_date_lte=timezone.now()).order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Poll
+    template_name = 'polls/detail.html'
 
 
-	return HttpResponse(template.render(context))
-
-
-# def index(request):
-#     latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
-#     context = {'latest_poll_list': latest_poll_list}
-#     return render(request, 'polls/index.html', context)
-
-
-def detail(request, poll_id):
-    try:
-        poll = Poll.objects.get(pk=poll_id)
-    except Poll.DoesNotExist:
-        raise Http404
-
-    #poll = get_object_or_404(Poll, pk=poll_id)    
-    return render(request, 'polls/detail.html', {'poll': poll})
-
-# def results(request, poll_id):
-#     return HttpResponse("You're looking at the results of poll %s." % poll_id)
-
-def results(request, poll_id):
-	poll = get_object_or_404(Poll, pk=poll_id)
-	return render(request, 'polls/results.html', {'poll': poll})
+class ResultsView(generic.DetailView):
+	model = Poll
+	template_name = 'polls/results.html'
 
 def vote(request, poll_id):
 	p = get_object_or_404(Poll, pk=poll_id)
